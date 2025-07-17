e498796a7f0227f0d86be827675bbe6c69c324d5
lucas-dias7 <w7.lucasdias@gmail.com>
2025-07-17 16:57:48 -0300

    add funcao para comprar determinado tipo de ingresso

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso').value;
-    let qtd = document.getElementById('qtd').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
+    
+    if (tipo == 'pista') {
+        comprarPista(qtd);
+    }
+}
 
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade de ingressos disponiveis insuficientes para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
 }
